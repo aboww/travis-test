44b70faf967c44baae9840216229ff997a996e6a
bla <d@asdf.com>
2018-01-19 20:38:20 +0000

    push reports

--- push_reports.sh
+++ push_reports.sh
@@ -2,7 +2,7 @@
 git init travis-reports
 cd travis-reports/
 git remote add origin https://${GHTOKEN}@github.com/aboww/aboww.github.io.git
-#1 2 3 2 w
+#1 2 3 2 w a
 git pull --depth=1 origin master
 cp ../target/site -r ./
 git add -A
