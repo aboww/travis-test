98e1f174a413444ee7431567cf5ae600a0c94d24
bla <d@asdf.com>
2018-01-19 18:11:51 +0000

    push reports

--- push_reports.sh
+++ push_reports.sh
@@ -3,7 +3,7 @@ git init travis-reports
 cd travis-reports/
 git remote add origin https://804c84ebd02ddfb3ec2bd2c79ac3b89e89c728d1@github.com/aboww/aboww.github.io.git
 git pull --depth=1 origin master
-cp ../../target/site -r ./
+cp ../target/site -r ./
 git add -A
 git config user.email celikjant@protonmail.com
 git config user.name travis-baba
