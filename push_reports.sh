d2ad805364fca459b10c8d0985b0166a54a46ca5
bla <d@asdf.com>
2018-01-19 20:25:09 +0000

    push reports

--- push_reports.sh
+++ push_reports.sh
@@ -2,7 +2,7 @@
 git init travis-reports
 cd travis-reports/
 git remote add origin https://f5ba2192cb1162834a252e09080c333589dac2af@github.com/aboww/aboww.github.io.git
-#1 2
+#1 2 3
 git pull --depth=1 origin master
 cp ../target/site -r ./
 git add -A
