997a31f07aa95b140783b892495cec24ea5b7042
bla <d@asdf.com>
2018-01-23 09:22:17 +0000

    update

--- push_reports.sh
+++ push_reports.sh
@@ -5,11 +5,13 @@ git remote add origin https://${GHTOKEN}@github.com/aboww/aboww.github.io.git
 #1 2 3 2 w a
 git pull --depth=1 origin master
 
+cd ..
 export COMMIT_ID=$(git rev-parse HEAD)
 export COMMIT_SHORT=${COMMIT_ID:0:8}
 export BUILD_DATE_TIME=$(git rev-parse HEAD | git show -s --format=%ci)
-sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/aboww/aboww.github.io/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"'</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site/surefire-report.html">Test reports</a><br>^g' index.html
+sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/aboww/travis-test/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"'</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site/surefire-report.html">Test reports</a><br>^g' index.html
 
+cd travis-reports/
 cp ../target/site -r ./
 git add -A
 git config user.email celikjant@protonmail.com
