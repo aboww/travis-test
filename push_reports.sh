233273d092c07a5bffb71e7b90717861b4e22180
bla <d@asdf.com>
2018-01-23 09:10:32 +0000

    update

--- push_reports.sh
+++ push_reports.sh
@@ -8,7 +8,7 @@ git pull --depth=1 origin master
 export COMMIT_ID=$(git rev-parse HEAD)
 export COMMIT_SHORT=${COMMIT_ID:0:8}
 export BUILD_DATE_TIME=$(git rev-parse HEAD | git show -s --format=%ci)
-sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/aboww/aboww.github.io/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"'</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site/surefire-report.html">Test reports</a><br>^g' index
+sed -i 's^<!--template-->^<!--template-->\n<a href="https://github.com/aboww/aboww.github.io/commit/'"$COMMIT_ID"'">'"$COMMIT_SHORT"'</a><span> :  ('"$BUILD_DATE_TIME"')</span> <a href="site/surefire-report.html">Test reports</a><br>^g' index.html
 
 cp ../target/site -r ./
 git add -A
